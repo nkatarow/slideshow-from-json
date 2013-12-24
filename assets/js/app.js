10ae6a34318be872deb06915fbb21d167d019ddd
Nick Katarow <nkatarow@ubermind.com>
2013-12-24 14:32:08 -0800

    adding initial project files

---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+/*
+
+    Main JavaScript
+    VERSION 0.0.1
+    AUTHORS: Nick Katarow
+
+    DEPENDENCIES:
+    - jQuery 1.7.2
+
+    TO DO:
+    - AJAX Failure Method
+*/
+
+var IS = window.IS || {};
+
+window.IS = {
+    init: function () {
+        var self = this,
+            current = 0,
+            slideshowData = '/assets/json/test.json',
+            queue = self.getImages(slideshowData);
+
+        // Start looping through the array
+        self.photoLoop(queue, current);
+    }, // END init
+
+    getImages: function (slideshowData) {
+        var queue = [],
+            dataObj;
+
+        // Reach out to JSON and assign data to dataObj variable
+        $.ajax({
+            url: slideshowData,
+            async: false,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                dataObj = data;
+            }
+        });
+
+        // For every image in object, add URL to queue array
+        for (var key in dataObj.data.images) {
+            queue.push(dataObj.data.images[key].link);
+        }
+
+        return queue;
+    }, // END getImages
+
+    imgEmbed: function (imgURL) {
+        $('#slideshow').append('<li><span><img src="' + imgURL + '"></span></li>');
+    }, // END imgEmbed
+
+    photoLoop: function (queue, current) {
+        var self = this;
+
+        console.log(current);
+
+        // If the list already has items in it...
+        if ($('#slideshow').children('li').length) {
+
+            if (current < queue.length - 1) {
+
+                current++;
+
+                $("#slideshow li:first-child").fadeOut( 1300, function() {
+                    self.removePhoto($('#slideshow li:first-child'), queue, current);
+
+                    // Embeds next image in queue to the bottom of the list
+                    self.imgEmbed(queue[current]);
+                });
+
+
+            // If we're the last image
+            } else if (current === queue.length - 1) {
+
+                current = 0;
+                $("#slideshow li:first-child").fadeOut( 1300, function() {
+                    self.removePhoto($('#slideshow li:first-child'), queue, current);
+
+                    // Embeds next image in queue to the bottom of the list
+                    self.imgEmbed(queue[current]);
+                });
+
+            } else {
+                console.log('This shouldn\'t even happen... WTF');
+            }
+
+            setTimeout (function(){
+                self.photoLoop(queue, current);
+            }, 10000);
+
+
+        } else {
+            self.imgEmbed(queue[0]);
+            self.imgEmbed(queue[1]);
+
+            current++;
+
+            setTimeout (function(){
+                self.photoLoop(queue, current);
+            }, 10000);
+        }
+    }, // END photoLoop
+
+    removePhoto: function (currentImg, queue, current) {
+        var self = this;
+
+        currentImg.remove();
+    } // END transition
+};
